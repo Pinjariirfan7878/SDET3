b0f5c3da5708ada16518e7530b4d4949850ba7a6
pinja <pinja@DESKTOP-3BU923C>
2022-07-29 14:48:08 +0530

    Sdet3 update

---
+++ src/test/java/git1/Git_1.java
@@ -0,0 +1,10 @@
+package git1;
+
+public class Git_1 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println(" GIt_1");
+	}
+
+}
---
+++ src/test/java/git1/Git_2.java
@@ -0,0 +1,10 @@
+package git1;
+
+public class Git_2 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println(" GIt_1");
+	}
+
+}
